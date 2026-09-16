5232ca62ca2efc1172d80f68a6827c7d7eb28ed6
Talebul Islam <talifanwarkhan@gmail.com>
2026-09-17 03:32:47 +0530

    Fix PRODUCTION_API_URL formatting in config.js

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@ const LOCAL_API_URL = "http://localhost:8000";
 
 // This must be the public URL of the deployed Node server, not the GitHub Pages URL.
 // Example: "https://net-angular-api.onrender.com"
-const PRODUCTION_API_URL = "https://talebulislam.github.io/.Net-Angular/";
+const PRODUCTION_API_URL = "https://talebulislam.github.io/.Net-Angular";
 
 const isLocal = ["localhost", "127.0.0.1"].includes(window.location.hostname);
 const apiBaseUrl = isLocal ? LOCAL_API_URL : PRODUCTION_API_URL;
